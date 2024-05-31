e7418626ddb1a143788395d10f8887f0b1129b37
QadisChaudhry <qadis.chaudhry@gmail.com>
2024-05-31 17:12:58 -0400

    minor changes

--- keymap.c
+++ keymap.c
@@ -11,16 +11,16 @@ enum charybdis_keymap_layers {
 enum custom_keycodes {
     UNDO = SAFE_RANGE,
     REDO,
-    USR_COPY,
-    USR_PASTE,
+    COPY,
+    PASTE,
     BACK,
     FORWARD,
     NEWTAB,
     CLOSETAB
 };
 
-#define L2_PASTE LT(_Layer2, USR_PASTE)
-#define L3_COPY LT(_Layer3, USR_COPY)
+#define L2_DEL LT(_Layer2, KC_DEL)
+#define L3_TAB LT(_Layer3, KC_TAB)
 
 #define TABLEFT S(C(KC_TAB))
 #define TABRIGHT C(KC_TAB)
@@ -31,21 +31,21 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_Layer1] = LAYOUT(
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       REDO,    KC_BTN2, KC_TAB,  KC_LCTL, NEWTAB,  CLOSETAB,
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       UNDO,    KC_BTN1, DRGSCRL, KC_BTN3, KC_LSFT, XXXXXXX,
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_ESC,  KC_ENT,  KC_DEL,  SNIPING, XXXXXXX,
-                                  XXXXXXX, XXXXXXX, XXXXXXX,       L3_COPY, L2_PASTE
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       REDO,    KC_BTN2, COPY,    PASTE,   NEWTAB,  CLOSETAB,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       UNDO,    KC_BTN1, KC_BTN3, KC_LSFT, KC_LCTL, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, DRGSCRL, KC_ESC,  KC_ENT,  SNP_TOG, XXXXXXX,
+                                  XXXXXXX, XXXXXXX, XXXXXXX,       L3_TAB,  L2_DEL
     ),
 
     [_Layer2] = LAYOUT(
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       DPI_MOD, BACK,     TABLEFT, TABRIGHT, FORWARD, XXXXXXX,
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, DLEFT,   DRIGHT,
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       QK_BOOT, C(KC_UP), XXXXXXX, XXXXXXX,  KC_F10,  XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       DPI_MOD, BACK,     TABLEFT,    TABRIGHT, FORWARD, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_LEFT, KC_DOWN,  KC_UP,      KC_RIGHT, DLEFT,   DRIGHT,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, C(KC_UP), C(KC_DOWN), XXXXXXX,  XXXXXXX, XXXXXXX,
                                   XXXXXXX, XXXXXXX, XXXXXXX,       _______, _______
     ),
 
     [_Layer3] = LAYOUT(
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_7, KC_8, KC_9, KC_0,    KC_PEQL,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       QK_BOOT, KC_7, KC_8, KC_9, KC_0,    KC_PEQL,
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_DOT,  KC_4, KC_5, KC_6, KC_PPLS, KC_MINS,
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_COMM, KC_1, KC_2, KC_3, KC_PSLS, KC_PAST,
                                   XXXXXXX, XXXXXXX, XXXXXXX,       _______, _______
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case L2_PASTE:
+        case L2_DEL:
             return TAPPING_TERM - 110;
-        case L3_COPY:
+        case L3_TAB:
             return TAPPING_TERM - 110;
         default:
             return TAPPING_TERM;
@@ -89,33 +89,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     unregister_code(KC_LSFT);
                     unregister_code(KC_LGUI);
                 } else {
-                    unregister_code(KC_LCTL);
+                    register_code(KC_LCTL);
                     tap_code(KC_Y);
                     unregister_code(KC_LCTL);
                 }
             }
             return false;
-        case USR_COPY:
+        case COPY:
             if (record->event.pressed) {
                 if (host_os == OS_MACOS || host_os == OS_IOS) {
-                    unregister_code(KC_LGUI);
+                    register_code(KC_LGUI);
                     tap_code(KC_C);
                     unregister_code(KC_LGUI);
                 } else {
-                    unregister_code(KC_LCTL);
+                    register_code(KC_LCTL);
                     tap_code(KC_C);
                     unregister_code(KC_LCTL);
                 }
             }
             return false;
-        case USR_PASTE:
+        case PASTE:
             if (record->event.pressed) {
                 if (host_os == OS_MACOS || host_os == OS_IOS) {
-                    unregister_code(KC_LGUI);
+                    register_code(KC_LGUI);
                     tap_code(KC_V);
                     unregister_code(KC_LGUI);
                 } else {
-                    unregister_code(KC_LCTL);
+                    register_code(KC_LCTL);
                     tap_code(KC_V);
                     unregister_code(KC_LCTL);
                 }
